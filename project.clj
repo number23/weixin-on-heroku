5f0e10c90d9cfda909f2fafec9a4a6e87bf879a6
number23 <number23.cn@gmail.com>
2013-03-27 16:06:32 +0800

    fix project.xml

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Template for new weixin robot on Heroku platform."
   :url "https://github.com/number23/weixin-on-heroku"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-clojars "0.9.1"]])
